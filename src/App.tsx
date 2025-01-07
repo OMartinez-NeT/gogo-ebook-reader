8641173dacd2b61500975cd2d75cf88e843a7488
jasmineqb <jasmine.english.editor@gmail.com>
2025-01-07 12:13:35 -1000

    Added highlight display and pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useCallback } from "react";
+import ReactPaginate from 'react-paginate';
 import ePub from "epubjs";
 //import "./App.css";
 import "./examples.css";
@@ -11,7 +12,12 @@ function App() {
   const [showPrev, setShowPrev] = useState(true);
   const [showNext, setShowNext] = useState(true);
   const [isHighlightEnabled, setHighlightEnabled] = useState(false);
+  const [highlights, setHighlights] = useState([]);
   const [isCommentEnabled, setCommentEnabled] = useState(false);
+  const [highlightPageNum, setHighlightPageNum] = useState(0);
+
+  const hihglightPerPage = 5;
+  const pagesVisited = highlightPageNum * hihglightPerPage;
 
   useEffect(() => {
     const bookUrl = ePub("./src/Data/example.epub");
@@ -63,6 +69,7 @@ function App() {
     };
   }, [book])
 
+  //---------------- Navigation
   const handleSectionChange = (e) => {
     const url = e.target.value;
     rendition.display(url);
@@ -77,20 +84,73 @@ function App() {
     rendition.next();
   }
 
+  const displayTOC = toc.map((section, index) => (
+    <option key={index} value={section.href}>
+      {section.label}
+    </option>
+  ))
+
+  //---------------- Highlights
+  const handleHighlight = () => {
+    if (isHighlightEnabled) {
+      rendition.off('selected', handleHighlightingText);
+      rendition.off('selected', handleHighlightData);
+      setHighlightEnabled(false);
+    } else {
+      rendition.on('selected', handleHighlightingText);
+      rendition.on('selected', handleHighlightData);
+      setHighlightEnabled(true);
+    }
+  };
+
   const handleHighlightingText = useCallback((cfiRange, contents) => {
     rendition.annotations.highlight(cfiRange);
     contents.window.getSelection().removeAllRanges();
   }, [rendition]);
 
-  const handleHighlight = () => {
-    if (isHighlightEnabled) {
-      rendition.off('selected', handleHighlightingText);
-      setHighlightEnabled(false);
-    } else {
-      rendition.on('selected', handleHighlightingText);
-      setHighlightEnabled(true);
+  const handleHighlightData = useCallback(async (cfiRange) => {
+    const range = await book.getRange(cfiRange);
+
+    if (range) {
+      const text = range.toString();
+
+      const data = {
+        range: cfiRange,
+        text: text
+      }
+
+      setHighlights((prevData) => [...prevData, data]);
     }
+
+  }, [book])
+
+  const handleRemoveHighlight = (cfiRange) => {
+    rendition.annotations.remove(cfiRange, 'highlight');
+    setHighlights((prevData) => prevData.filter((data) => data.range !== cfiRange));
   };
+
+  const handleDisplayHighlight = (cfiRange) => {
+    rendition.display(cfiRange);
+  };
+
+  const displayHighlight = highlights.slice(pagesVisited, pagesVisited + hihglightPerPage)
+    .map((data, index) => (
+      <li className='highlight-display' key={index}>
+        <span>{data.text}</span>
+        <br />
+        <br />
+        <button className='btn' onClick={() => handleDisplayHighlight(data.range)}>Go to Page</button>
+        <button className='btn' onClick={() => handleRemoveHighlight(data.range)}>Delete</button>
+      </li>
+    ))
+
+  const highlightPageCount = Math.ceil(highlights.length / hihglightPerPage);
+
+  const handleHighlightChangePage = ({ selected }) => {
+    setHighlightPageNum(selected);
+  };
+
+  //------------------ Comments
 
   const handleCommentingText = useCallback((cfiRange, contents) => {
     const selectedText = contents.window.getSelection().toString();
@@ -115,25 +175,28 @@ function App() {
     <>
       <div className='right-column'>
         <select className='toc' value={selectedSection} onChange={handleSectionChange}>
-          {toc.map((section, index) => (
-            <option key={index} value={section.href}>
-              {section.label}
-            </option>
-          ))}
+          {displayTOC}
         </select>
+          {displayHighlight}
+          <ReactPaginate
+            previousLabel={"Previous"}
+            nextLabel={"Next"}
+            pageCount={highlightPageCount}
+            onPageChange={handleHighlightChangePage}
+            containerClassName={"paginationBttns"}
+            previousLinkClassName={"previousBttn"}
+            nextLinkClassName={"nextBttn"}
+            disabledClassName={"paginationDisabled"}
+            activeClassName={"paginationActive"}
+          />
       </div>
       <div className='left-column'>
         <div className='top-menu'>
           <button
-            className={isHighlightEnabled ? 'btn on' : 'btn off'}
-            onClick={handleHighlight}
-          >
+            className={isHighlightEnabled ? 'btn on' : 'btn off'} onClick={handleHighlight}>
             {isHighlightEnabled ? 'Highlights: On' : 'Highlights: Off'}
           </button>
-          <button
-            className={isCommentEnabled ? 'btn on' : 'btn off'}
-            onClick={handleComment}
-          >
+          <button className={isCommentEnabled ? 'btn on' : 'btn off'} onClick={handleComment}>
             {isCommentEnabled ? 'Comment: On' : 'Comment: Off'}
           </button>
         </div>
